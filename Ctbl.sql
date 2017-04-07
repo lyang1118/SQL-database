165125ca9d199dc223d305efa17e7917dc02c0a1
lyang1118 <lyang1118@yahoo.com>
2017-04-07 14:42:42 +0800

    Add files via upload

--- Ctbl.sql
+++ Ctbl.sql
@@ -1,2 +1,6 @@
 create table customer 
-	(id int IDENTITY(1,1), cname varchar(30), cphone varchar(10));
+	(id int IDENTITY(1,1), cname varchar(30), cphone varchar(10), createdate datetime);
+
+create table rmaorder
+	(id int IDENTITY(1,1) rorderno varchar(10), pno varchar(10), pserno varchar(10), rdate datetime,
+	cdate datetime, fdate datetime);
